2dbc6ddbfdb5126c4b453d14e6a3d2d40f7fcefc
Chris Jenkins <cdpjenkins@gmail.com>
2024-12-28 23:43:04 +0000

    Number guessing game from chapter 2 of the book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
@@ -5,7 +6,7 @@ fn main() {
     println!("Hello, Rust!");
     println!("Lalalalala!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
     println!("Guess the numebr!!!");
     loop {
@@ -15,7 +16,23 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {}", guess);
-        println!("Too bad the number is, in fact, {}.", secret_number);
+        let ur_guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Bad stuff happened that we don't yet know how to deal with");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", ur_guess);
+
+        match ur_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("Your guess appears to be correct!");
+                break;
+            }
+        }
     }
 }
