5b036cecaa937e8f23f97607b3140a410d8e29aa
Chris Jenkins <cdpjenkins@gmail.com>
2024-12-29 14:17:21 +0000

    Learning about data types.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,27 @@ fn main() {
     println!("Hello, Rust!");
     println!("Lalalalala!");
 
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    println!("{} {} {}", c, z, heart_eyed_cat);
+
+    let i: u64 = 0x1234567890abcdef;
+    println!("{:x}", i);
+
+    let a_tuple = (1isize, '😻', 1.23456);
+    println!("{:?}", a_tuple);
+
+    let an_array = [1, 2, 3, 4, 5];
+    println!("{:?}", an_array);
+
+    let string_array = ["Hello", "World", "!!"];
+    println!("{:?}", string_array);
+    println!("{} {}{}", string_array[0], string_array[1], string_array[2]);
+
+    let array_with_repeating_values = ["hello!!!!";5];
+    println!("{:?}", array_with_repeating_values);
+
     let secret_number = rand::thread_rng().gen_range(1..100);
 
     println!("Guess the numebr!!!");
