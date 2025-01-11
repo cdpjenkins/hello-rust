b4d4e3afc5270911fceab4b2c9e1771db8e4a72b
Chris Jenkins <cdpjenkins@gmail.com>
2025-01-11 19:56:04 +0000

    Add a test for parse_config

--- src/grep.rs
+++ src/grep.rs
@@ -4,8 +4,7 @@ pub fn minigrep() {
 
     dbg!(&args);
 
-    let query = &args[2];
-    let file_path = &args[3];
+    let (query, file_path) = parse_config(&args);
 
     println!("Query: {}", query);
     println!("File path: {}", file_path);
@@ -14,4 +13,34 @@ pub fn minigrep() {
         .expect("Unable to read file");
 
     println!("File contents: {}", file_contents);
+}
+
+fn parse_config(args: &[String]) -> (&String, &String) {
+    let query = &args[2];
+    let file_path = &args[3];
+
+    (query, file_path)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::grep::parse_config;
+
+    #[test]
+    // Note I wish I could move args into the parse_config function and therefore not have to
+    // declare args in a let block. But my Rust is not yet good enough to know how to do that.
+    fn can_parse_config_from_command_line_args() {
+        let args = args_of(&["target/debug/hello-rust", "grep", "This", "hello.txt"]);
+        let (query, file_path) = parse_config(&args);
+        assert_eq!(query, &"This".to_string());
+        assert_eq!(file_path, &"hello.txt".to_string());
+    }
+
+    fn args_of(vec2: &[&str]) -> Vec<String> {
+        let vec1 = vec2
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        vec1
+    }
 }
