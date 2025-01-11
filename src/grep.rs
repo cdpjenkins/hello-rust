8871aea5e2878c7ea0c6820a152118278e62a4f2
Chris Jenkins <cdpjenkins@gmail.com>
2025-01-11 20:06:09 +0000

    Introduce Config structure

--- src/grep.rs
+++ src/grep.rs
@@ -4,22 +4,27 @@ pub fn minigrep() {
 
     dbg!(&args);
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Query: {}", query);
-    println!("File path: {}", file_path);
+    println!("Query: {}", config.query);
+    println!("File path: {}", config.file_path);
 
-    let file_contents = std::fs::read_to_string(file_path)
+    let file_contents = std::fs::read_to_string(config.file_path)
         .expect("Unable to read file");
 
     println!("File contents: {}", file_contents);
 }
 
-fn parse_config(args: &[String]) -> (&String, &String) {
-    let query = &args[2];
-    let file_path = &args[3];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[2].clone();
+    let file_path = args[3].clone();
+
+    Config { query, file_path }
 }
 
 #[cfg(test)]
@@ -27,13 +32,10 @@ mod tests {
     use crate::grep::parse_config;
 
     #[test]
-    // Note I wish I could move args into the parse_config function and therefore not have to
-    // declare args in a let block. But my Rust is not yet good enough to know how to do that.
     fn can_parse_config_from_command_line_args() {
-        let args = args_of(&["target/debug/hello-rust", "grep", "This", "hello.txt"]);
-        let (query, file_path) = parse_config(&args);
-        assert_eq!(query, &"This".to_string());
-        assert_eq!(file_path, &"hello.txt".to_string());
+        let config = parse_config(&args_of(&["target/debug/hello-rust", "grep", "This", "hello.txt"]));
+        assert_eq!(config.query, "This".to_string());
+        assert_eq!(config.file_path, "hello.txt".to_string());
     }
 
     fn args_of(vec2: &[&str]) -> Vec<String> {
